53728006394e06e0f8d15589d474212ea3da571b
Enache Lucian Adrian <enachelucianadrian@gmail.com>
2021-11-26 22:24:41 +0200

    comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,7 @@ package ro.java.ctrln;
 public class Main {
 
     public static void main(String[] args) {
-
+        //comentariu
         System.out.println("Aceasta este prima mea aplicatie Java");
         System.out.println("De fapt e a doua");
 
