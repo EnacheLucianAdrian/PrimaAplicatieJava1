8d1cf63e9a8859f0d29f779df2ce8b3c04d38b38
Enache Lucian Adrian <enachelucianadrian@gmail.com>
2021-11-26 22:57:25 +0200

    comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         //comentariu
+        //sfdsgf
+        //rdfgjh
         System.out.println("Aceasta este prima mea aplicatie Java");
         System.out.println("De fapt e a doua");
 
