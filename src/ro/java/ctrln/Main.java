967444ce851616998993aa7da563972426d68d53
Enache Lucian Adrian <enachelucianadrian@gmail.com>
2021-11-26 06:36:40 +0200

    am scris a doua linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Aceasta este prima mea aplicatie Java");
+        System.out.println("De fapt e a doua");
 
     }
 }
