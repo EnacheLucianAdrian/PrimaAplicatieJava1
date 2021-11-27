ab4a81309473ba38134efd64dbe6a7d7dc09e098
EnacheLucianAdrian <enachelucianadrian@gmail.com>
2021-11-27 23:57:10 +0200

    am importat din nou proiectul in /desktop/ctrl+n

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,13 +3,7 @@ package ro.java.ctrln;
 public class Main {
 
     public static void main(String[] args) {
-        //comentariu
-        /*
-        teste bla bla...am mai facut asta la inceput
-         */
-        /* sfdsgf
-        //test pt push
-        //rdfgjh */
+
         System.out.println("Aceasta este prima mea aplicatie Java");
         System.out.println("De fapt e a doua");
 
